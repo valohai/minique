a66d6cfe063f77ae8c6da5737feed89031cb4bae
Aarni Koskela <akx@iki.fi>
2019-12-03 12:27:41 +0200

    Clean out bare except:s

--- minique/work/worker.py
+++ minique/work/worker.py
@@ -53,5 +53,5 @@ class Worker:
                 self.tick()
             except KeyboardInterrupt:
                 break
-            except:  # noqa
+            except Exception:  # noqa
                 self.log.error('Unexpected worker tick error', exc_info=True)
