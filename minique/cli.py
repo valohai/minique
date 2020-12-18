7e5143191228f72d1a6317a018983a17e6453768
Aarni Koskela <akx@iki.fi>
2020-12-18 12:46:50 +0200

    Add test for CLI

--- minique/cli.py
+++ minique/cli.py
@@ -11,15 +11,19 @@ def get_parser():
     parser.add_argument("-u", "--redis-url", required=True)
     parser.add_argument("-q", "--queues", nargs="+", required=True)
     parser.add_argument("--allow-callable", nargs="+", required=True)
+    parser.add_argument("--single-tick", action="store_true")
     return parser
 
 
-def main():
+def main(argv=None):
     parser = get_parser()
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
     logging.basicConfig(datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO)
     redis = StrictRedis.from_url(args.redis_url)
     worker = Worker.for_queue_names(redis=redis, queue_names=args.queues)
     worker.allowed_callable_patterns = set(args.allow_callable)
     worker.log.info("Worker initialized")
-    worker.loop()
+    if args.single_tick:
+        worker.tick()
+    else:
+        worker.loop()
---
+++ minique_tests/test_cli.py
@@ -0,0 +1,21 @@
+from minique.api import enqueue
+from minique.cli import main
+
+
+def test_cli(redis, redis_url, random_queue_name):
+    kwargs = {"a": 10, "b": 15}
+    job = enqueue(
+        redis, random_queue_name, "minique_tests.jobs.sum_positive_values", kwargs
+    )
+    main(
+        [
+            "-u",
+            redis_url,
+            "-q",
+            random_queue_name,
+            "--allow-callable",
+            "minique_tests.*",
+            "--single-tick",
+        ]
+    )
+    assert job.result == 25
