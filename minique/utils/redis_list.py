91ae32ec63c3e0e9190e656574ab8df591c42ea6
Aarni Koskela <akx@iki.fi>
2022-10-06 13:28:43 +0300

    redis_list: do less queries

--- minique/utils/redis_list.py
+++ minique/utils/redis_list.py
@@ -21,18 +21,22 @@ def read_list(
     :param last_n: Attempt to only read the last N lines.
     :return:
     """
-    if not redis_conn.exists(key):
+    # LLEN returns zero for a non-existent key,
+    # and there would be nothing to iterate from an empty
+    # list anyway, so we can do one query less.
+    list_len = redis_conn.llen(key)
+    if list_len == 0:
         return
 
     if chunk_size <= 0:
         chunk_size = 4096
 
     if last_n and last_n > 0:
-        offset = max(0, redis_conn.llen(key) - last_n)
+        offset = max(0, list_len - last_n)
     else:
         offset = 0
 
-    while offset < redis_conn.llen(key):
+    while offset < list_len:
         # Regarding that - 1 there, see this from https://redis.io/commands/lrange:
         # > Note that if you have a list of numbers from 0 to 100, LRANGE list 0 10
         # > will return 11 elements, that is, the rightmost item is included.
